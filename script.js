de0494d3922c364bc7d68f029f28f1019eeca277
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-07-16 17:13:41 -0500

    Updated style.css and script.js files

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const viewsEl = document.getElementById("views");
+const priceEl = document.getElementById("price");
+const slider = document.getElementById("slider");
+const checkbox = document.getElementById("checkbox");
+const monthlyPrices = [8, 12, 16, 24, 36];
+const viewsArr = [
+    "10K pageviews",
+    "50K pageviews",
+    "100K pageviews",
+    "500K pageviews",
+    "1M pageviews",
+],
+const yearlyDiscount = 0.25;
+
+slider.addEventListener("input", (event));
+
+
+
+
+
+
+
+
